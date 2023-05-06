701b37711f6de172a6109a88b1889ca48884f6dd
MetaInsight <9798772@qq.com>
2023-05-06 18:06:04 +0800

    second commit

--- deploy.sh
+++ deploy.sh
@@ -1,37 +1,42 @@
 #!/bin/bash
 echo "深度学习环境配置一键脚本"
+echo "0.Install ALL"
+echo "1.Install miniconda"
+echo "2.Install jupyter lab"
+echo "3.Install miniconda"
+echo "4.Install pytorch"
 
-# 更新
-sudo apt update && sudo apt upgrade -y
+# # 更新
+# sudo apt update && sudo apt upgrade -y
 
-# # 安装miniconda (重定向法)
-# wget -P ~ -N https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+# # # 安装miniconda (重定向法)
+# # wget -P ~ -N https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+# # echo "正在安装 miniconda......"
+# # {
+# # 	echo ${KEYCODE_ENTER};
+# # 	echo "yes"; 
+# # 	echo ${KEYCODE_ENTER};
+# #     echo "yes";
+# # } |  bash ~/Miniconda3-latest-Linux-x86_64.sh 1>./Anaconda.log 2>&1
+# # echo "miniconda 安装成功！"
+
+# # 安装miniconda (官方静默安装)
 # echo "正在安装 miniconda......"
-# {
-# 	echo ${KEYCODE_ENTER};
-# 	echo "yes"; 
-# 	echo ${KEYCODE_ENTER};
-#     echo "yes";
-# } |  bash ~/Miniconda3-latest-Linux-x86_64.sh 1>./Anaconda.log 2>&1
+# wget -P ~ -N https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+# bash ~/Miniconda3-latest-Linux-x86_64.sh -b -p ~/miniconda3
+# # 初始化
+# source ~/miniconda3/bin/activate
+# conda init
+# source ~/.bashrc
 # echo "miniconda 安装成功！"
 
-# 安装miniconda (官方静默安装)
-echo "正在安装 miniconda......"
-wget -P ~ -N https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
-bash ~/Miniconda3-latest-Linux-x86_64.sh -b -p ~/miniconda3
-# 初始化
-source ~/miniconda3/bin/activate
-conda init
-source ~/.bashrc
-echo "miniconda 安装成功！"
+# # 安装jupyter lab
+# echo "正在安装 jupyter lab......"
+# conda install -y jupyterlab
+# conda install -y nb_conda_kernels
+# echo "jupyter lab 安装成功！"
 
-# 安装jupyter lab
-echo "正在安装 jupyter lab......"
-conda install -y jupyterlab
-conda install -y nb_conda_kernels
-echo "jupyter lab 安装成功！"
-
-# 安装pytorch
-echo "正在安装 pytorch......"
-conda install -y pytorch torchvision torchaudio pytorch-cuda=11.7 -c pytorch -c nvidia
-echo "pytorch 安装成功！"
+# # 安装pytorch
+# echo "正在安装 pytorch......"
+# conda install -y pytorch torchvision torchaudio pytorch-cuda=11.7 -c pytorch -c nvidia
+# echo "pytorch 安装成功！"
